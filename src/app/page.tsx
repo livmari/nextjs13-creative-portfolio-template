008ce3d79e6d91bc0261785f256d88f165d16dfb
Liv Mari <liv.alterskjaer@icloud.com>
2023-07-23 15:05:01 +0200

    display list of projects on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
-const HomePage = () => {
+import { gql } from '@apollo/client'
+import Link from 'next/link'
+import { nanoid } from 'nanoid'
+
+import { getClient } from '@/lib/client'
+
+const query = gql`
+  query Projects {
+    projects {
+      title
+      slug
+    }
+  }
+`
+
+const HomePage = async () => {
+  const { data } = await getClient().query({ query })
+
   return (
     <main>
-      <h1>Home page</h1>
+      <section>
+        <div>
+          {data.projects.map((project: { title: string; slug: string }) => (
+            <Link key={nanoid()} href={`/projects/${project.slug}`}>
+              {project.title}
+            </Link>
+          ))}
+        </div>
+      </section>
     </main>
   )
 }
