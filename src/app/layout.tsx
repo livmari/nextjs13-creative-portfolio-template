b005476905192f56eec9c6719fd13935f5276aaa
Liv Mari <liv.alterskjaer@icloud.com>
2023-07-23 13:49:31 +0200

    connect pages with nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import { PageNav } from '@/components'
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang={'en'}>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <PageNav />
+
+        {children}
+      </body>
     </html>
   )
 }
